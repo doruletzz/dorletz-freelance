e61309dfc00dc1d8a52ca9b76a7a33b2703fe423
doruletzz <dorosdorulucian@gmail.com>
2024-11-24 15:31:21 +0300

    feat: update

--- src/provider/posthog.tsx
+++ src/provider/posthog.tsx
@@ -5,6 +5,7 @@ import { PostHogProvider } from 'posthog-js/react'
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
     capture_pageview: false // Disable automatic pageview capture, as we capture manually
   })
 }
