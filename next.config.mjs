90cadce4fb7f5d3c27eb2adac36ea4eed94d9231
doruletzz <dorosdorulucian@gmail.com>
2024-11-25 12:56:00 +0300

    feat: update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["next-mdx-remote"],
+  experimental: { 
+    nftTracing: true 
+  }
   // i18n: {
   //   locales: ["en", "ro"],
   //   defaultLocale: "en",
